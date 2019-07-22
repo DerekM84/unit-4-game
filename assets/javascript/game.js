342a86548694764900ddc7252cc6ef0f2b8b0e54
Derek Miller <derekmlmt@yahoo.com>
2019-07-21 21:57:19 -0700

    Finished CSS and Javascript is debugged and slightly refactored.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 console.log("game.js linked");
 
 $(document).ready(function() {
-console.log("document ready");
+    console.log("document ready function");
 
 
 var wins = 0;
@@ -10,25 +10,42 @@ var totalScore = 0;
 
 function reset() {
     totalScore=0;
+    $(".net-worth").empty();
+    $(".gem-value").empty();
 }
-// Clicking any button will add the data-value to the total score
-$(".gem button").on("hover", function() {
+// Reset variables and clear numbers from boxes
+
+$(".reset-button").on("click", function() {
+        console.log("reset clicked");
+    reset();
+} );
+
+// Display gem value while hovering over one, and no value while not hovering.
+
+$(".gem").on("mouseenter", function() {
     var gemValue = parseInt($(this).attr("data-value"));
-    console.log(gemValue);
+        console.log("Hovering over a gem worth " + gemValue);
     $(".gem-value").empty();
     $(".gem-value").append(gemValue);
+});
+
+$(".gem").on("mouseleave", function() {
+    $(".gem-value").empty();
 })
+
+// Clicking any button will add the unique data-value to the total score and check to see if 100 has been reached yet
 
 $(".gem").on("click", function() {
     console.log("clicked a button worth " + $(this).attr("data-value"));
     totalScore = totalScore + parseInt($(this).attr("data-value"));
-    console.log(totalScore);
+        console.log(totalScore);
+
     $(".net-worth").empty();
     $(".net-worth").append(totalScore);
 
-    console.log(typeof totalScore);
-    if (totalScore === 100) {
+        console.log("totalScore is a " + typeof totalScore);
 
+    if (totalScore === 100) {
         wins++;
         $(".net-worth").empty();
         $(".wins").empty();
@@ -42,16 +59,6 @@ $(".gem").on("click", function() {
         $(".losses").empty();
         $(".losses").append(losses);
         reset();
-
     }
-
-} );
-
-
-$(".reset-button").on("click", function() {
-console.log("reset clicked");
-totalScore = 0;
-
-} );
-
+});
 });
